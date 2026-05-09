eaad9768bfc1b19124305575fb57d65dbb206a86
Alitaf <118794794+Alitaf@users.noreply.github.com>
2026-05-09 14:01:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -64,8 +64,16 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text("🔴 مشکلی در پردازش پیش آمد، لطفاً دوباره بپرسید.")
 
+async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = (
+        "لطفاً سوال خود را به صورت متنی ارسال کنید تا رکسانا بتواند شما را راهنمایی کند.\n\n"
+        "Please send your question in text format so Roxana can assist you."
+    )
+    await update.message.reply_text(text)
+
 if __name__ == '__main__':
     threading.Thread(target=run_health_check, daemon=True).start()
     app = Application.builder().token(os.getenv("TELEGRAM_TOKEN")).build()
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
+    app.add_handler(MessageHandler(filters.VOICE, handle_voice))
     app.run_polling(drop_pending_updates=True)
